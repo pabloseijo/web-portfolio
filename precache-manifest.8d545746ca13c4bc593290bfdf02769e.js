f450bd7442cc465678f72f8cd7c3cb1c0ade6fb6
Pablo Seijo <pgarsei510@gmail.com>
2025-04-05 20:43:53 +0200

    Updates

---
+++ precache-manifest.8d545746ca13c4bc593290bfdf02769e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf1f62ea08a96eaf9b497adce839a63",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "7a0a2d046758b97a25b4",
+    "url": "/web-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a2e18d4087eefc3ece86",
+    "url": "/web-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "7a0a2d046758b97a25b4",
+    "url": "/web-portfolio/static/js/2.59e3ab9e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/web-portfolio/static/js/2.59e3ab9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e18d4087eefc3ece86",
+    "url": "/web-portfolio/static/js/main.50e11b0c.chunk.js"
+  },
+  {
+    "revision": "9b4eab530a11fdedd1e2",
+    "url": "/web-portfolio/static/js/runtime-main.a8a217c2.js"
+  }
+]);
